b4f9ba75ffcecfcab57669f41abb13a7f5a2186b
Paul <office@campus-robo.at>
2026-06-21 08:59:54 +0200

    more scripts added

---
+++ scripts/transfer_code.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ssh resqbots@lotti2-pc.local
+source ~/.bashrc && \
+cd Lotti2.0 && \
+remro && \
+rm -r src/ && \
+exit 
+scp -r ~/Lotti2.0/src/ resqbots@Lotti2-PC.local:/home/resqbots/Lotti2.0/"
